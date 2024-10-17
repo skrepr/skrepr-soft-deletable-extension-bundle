1b0acc247d00ba7edee690ff1bbdd8cfda856ae7
Didier Peereboom Voller <didier@skrepr.com>
2024-10-17 10:25:57 +0200

    Testing

---
+++ src/EventListener/OnSoftDeleteEventSubscriber.php
@@ -0,0 +1,72 @@
+<?php
+
+
+use Doctrine\ORM\AbstractQuery;
+use Doctrine\ORM\UnitOfWork;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Doctrine\Persistence\ObjectManager;
+use Gedmo\SoftDeleteable\SoftDeleteableListener as GedmoSoftDeleteableListener;
+
+class OnSoftDeleteEventSubscriber
+{
+    protected function cascadeAssociatedObjects(object $eventObject, array $metaData, ObjectManager $objectManager): void
+    {
+        // Field name is set in the targetEntity class, when Entity1 as #[onSoftDelete()] on a property.
+        // We should grab the SoftDelete fieldName from Gedmo.
+        $className = $metaData['associatedTo'];
+        $propertyName = $metaData['associatedToProperty'];
+        $fieldName = $metaData['targetEntitySoftDeleteFieldName'];
+
+        // Actually update the entities, doing it this way won't cause memory problems.
+        $deletedAt = new \DateTimeImmutable();
+        var_dump('A1');
+        $objectManager->createQueryBuilder()
+            ->update($metaData['associatedTo'], 'e')
+            ->set("e.{$fieldName}", ':deletedAt')
+            ->andWhere("e.{$propertyName} = :eventObject")
+            ->andWhere("e.{$fieldName} IS NULL")
+            ->setParameter('eventObject', $eventObject->getId()->toBinary())
+            ->setParameter('deletedAt', $deletedAt->format('Y-m-d H:i:s'))
+            ->getQuery()
+            ->execute()
+        ;
+
+        // Grab all the id's that are going to be updated, so we can schedule them for update.
+        $objectsAssociatedToEventObject = $objectManager->createQueryBuilder()
+            ->select('e.id')
+            ->from($metaData['associatedTo'], 'e')
+            ->andWhere("e.{$propertyName} = :eventObject")
+            ->setParameter('eventObject', $eventObject)
+            ->getQuery()
+            ->toIterable(hydrationMode: AbstractQuery::HYDRATE_ARRAY)
+        ;
+
+        /**
+         * @var UnitOfWork $uow
+         */
+        $uow = $objectManager->getUnitOfWork();
+        // Use the getReference() method to fetch a partial object for each entity
+        foreach ($objectsAssociatedToEventObject as $row) {
+            $id = $row['id'] ?? null;
+
+            if (null === $id) {
+                continue;
+            }
+
+            $objectProxy = $objectManager->getReference($className, $id);
+            $objectManager->getEventManager()->dispatchEvent(
+                GedmoSoftDeleteableListener::PRE_SOFT_DELETE,
+                new LifecycleEventArgs($objectProxy, $objectManager)
+            );
+
+            $uow->scheduleExtraUpdate($objectProxy, [
+                $fieldName => [null, $deletedAt],
+            ]);
+
+            $objectManager->getEventManager()->dispatchEvent(
+                GedmoSoftDeleteableListener::POST_SOFT_DELETE,
+                new LifecycleEventArgs($objectProxy, $objectManager)
+            );
+        }
+    }
+}
---
+++ src/config/services.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+use Doctrine\ORM\Events;
+use Gedmo\SoftDeleteable\SoftDeleteableListener;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
+
+return static function (ContainerConfigurator $container) {
+    $container
+        ->services()
+        ->set(
+            'stichtingsd.soft_deletable_extension.metadata_factory',
+            StichtingSD\SoftDeleteableExtensionBundle\Mapping\MetadataFactory::class
+        )
+        ->arg(0, service('stichtingsd.softdeleteable_extension.cache'))
+    ;
+
+    $container
+        ->services()
+        ->set(
+            'stichtingsd.soft_deletable_extension.subscriber',
+            App\EventListener\OnSoftDeleteEventSubscriber::class,
+        )
+        ->call('setContainer', [service('service_container')])
+        ->arg(0, service('stichtingsd.soft_deletable_extension.metadata_factory'))
+        ->tag('doctrine.event_listener', ['event' => SoftDeleteableListener::PRE_SOFT_DELETE])
+    ;
+
+    $container
+        ->services()
+        ->set(
+            'stichtingsd.soft_deletable_extension.validator',
+            StichtingSD\SoftDeleteableExtensionBundle\EventListener\OnSoftDeleteValidatorEventSubscriber::class,
+        )
+        ->call('setContainer', [service('service_container')])
+        ->arg(0, service('stichtingsd.soft_deletable_extension.metadata_factory'))
+        ->tag('doctrine.event_listener', ['event' => Events::loadClassMetadata])
+    ;
+};
